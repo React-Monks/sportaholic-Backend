5a2ef4d1e928bea02315c68ffff41eb4a5765d42
yousef-alramli <ysabdys1996@gmail.com>
2021-09-26 14:27:32 +0300

    starter code

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 "use strict";
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require("cors");  
 app.use(cors());
 require("dotenv").config();
 const PORT = process.env.PORT;
 const sportsData = require("./data/data.json");
+const mongoose = require("mongoose");
+app.use(mongoose());
 app.listen(process.env.PORT, () =>
   console.log(`Server up on ${process.env.PORT}`)
 );
+const MONGO_SERVER= process.env.MONGO_SERVER
 
-// 
-// ------------------
-class Sport {
-  constructor( strFormat, strSportThumb, strSportDescription) {
-      (this.strFormat = strFormat),
-      (this.strSportThumb = strSportThumb),
-      (this.strSportDescription = strSportDescription)
-  }
-}
+mongoose.connect(`${MONGO_SERVER}/Book`, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get("/sports", (req, res) => {
+
+// class Sport {
+//   constructor( strFormat, strSportThumb, strSportDescription) {
+//       (this.strFormat = strFormat),
+//       (this.strSportThumb = strSportThumb),
+//       (this.strSportDescription = strSportDescription)
+//   }
+// }
+
+app.get("/football", (req, res) => {
   let strSport = req.strFormat;
 
-
-  // let arr = sportsData.sports.find((item) => {
-  //   return item.strSport.toLowerCase() === strSport;
-  // });
-// console.log(arr)
   let sportsDetails = sportsData.sports.map((i) => {
     return new Sport(
       i.strSport,
@@ -41,3 +40,5 @@ app.get("/sports", (req, res) => {
 });
 
 // ----------------------------
+
+app.get('/nutrition')

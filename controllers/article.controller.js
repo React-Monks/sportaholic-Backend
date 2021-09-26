01da6c3a2450251efeccccbb2472c212cc42a70e
Anas-Abuhatab <anasm.abuhatab@gmail.com>
2021-09-26 16:48:59 +0300

    DB Atlas Datad Added

---
+++ controllers/article.controller.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const {articleModel}=require('../models/article.model');
+
+const getArticleController = (req,res) =>{
+    articleModel.find().then(data=>{
+        res.json(data);
+    })
+}
+
+
+
+const creatArticleController = async(req,res) =>{
+    let dataArticle = req.body;
+    let newArticle =new articleModel(dataArticle);
+    newArticle.save();
+    let articalLest=await articleModel.find({})
+     res.status(201).json(articalLest)
+    
+}
+
+const deleteArticleController = (req,res)=>{
+    let articleID = req.params.id;
+    articleModel.findByIdAndDelete(articleID).then(()=>{
+        articleModel.find().then(data=>{
+            res.status(200).json(data);
+        })
+    })
+    
+}
+
+const updateArticleController =async (req,res)=>{
+    let articleID =  req.params.id;
+    let dataArticle = req.body;
+    
+    articleModel.findOne({_id:articleID}).then(article=>{
+
+        article.userName = dataArticle.userName;
+        article.userEmail =dataArticle.userEmail;
+        article.text =dataArticle.text;
+        article.save();
+    });
+     
+
+      let list=await articleModel.find()
+       res.status(201).json(list);
+    
+}
+
+
+module.exports={
+    getArticleController,
+    creatArticleController,
+    deleteArticleController,
+    updateArticleController
+}
--- server.js
+++ server.js
@@ -5,39 +5,29 @@ const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 const PORT = process.env.PORT;
-const sportsData = require("./data/data.json");
+const mongoose=require('mongoose')
+const { creatArticleController,
+        getArticleController, 
+        deleteArticleController,
+        updateArticleController} = require("./controllers/article.controller");
+
+app.use(express.json());
+const MONGO_SERVER = process.env.MONGO_SERVER;
+
+mongoose.connect(`${MONGO_SERVER}`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.status(200).send('iam working')
+})
+
 app.listen(process.env.PORT, () =>
   console.log(`Server up on ${process.env.PORT}`)
 );
 
-// 
-// ------------------
-class Sport {
-  constructor( strFormat, strSportThumb, strSportDescription) {
-      (this.strFormat = strFormat),
-      (this.strSportThumb = strSportThumb),
-      (this.strSportDescription = strSportDescription)
-  }
-}
-
-app.get("/sports", (req, res) => {
-  let strSport = req.strFormat;
-
-
-  // let arr = sportsData.sports.find((item) => {
-  //   return item.strSport.toLowerCase() === strSport;
-  // });
-// console.log(arr)
-  let sportsDetails = sportsData.sports.map((i) => {
-    return new Sport(
-      i.strSport,
-      i.strFormat,
-      i.strSportThumb,
-      i.strSportDescription
-    );
-  });
-
-  res.status(200).json(strSport);
-});
-
 // ----------------------------
+
+
+app.get('/article',getArticleController);
+app.post('/createArticle',creatArticleController);
+app.delete('/deleteArticle/:id',deleteArticleController);
+app.put('/updateArticle/:id',updateArticleController)

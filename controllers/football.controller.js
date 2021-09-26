7c1fffa995ebfc0cd81d65301329c52e77a862b2
yousef-alramli <ysabdys1996@gmail.com>
2021-09-26 14:28:52 +0300

    starter code

--- controllers/football.controller.js
+++ controllers/football.controller.js
@@ -1,21 +1,10 @@
-// "use strict";
+"use strict";
 
-// const axios= require("axios");
+const axios= require("axios");
 
-// let sportsInformation  =async (req,res)=>{
-//     let strSport= req.query.strSport;
-//     let url =`https://www.thesportsdb.com/api/v1/json/1/all_sports.php&query=${strSport}`;
-// let sports= await axios.get(url)
-// let  sportsData= sports.data.sports.map((i)=>{
-//     return new Sport (
-//         i.strSport,
-//         i.strFormat,
-//         i.strSportThumb,
-//         i.strSportDescription
+let footballController = (req, res) => {
 
-//     )
-// })
-// res.status(200).json(sportsData)
-// }
-
-// module.exports= {sportsInformation}
+    bookModel.find().then(data => {
+        res.status(200).json(data)
+    })
+}
